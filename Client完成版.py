6772186cddbfcc957c69cb4f49b9e5950296c540
Llz1415 <90233785+LLZ14@users.noreply.github.com>
2022-12-02 16:28:33 +0800

    Update Client完成版.py

--- Client完成版.py
+++ Client完成版.py
@@ -396,5 +396,96 @@ if __name__ == '__main__':
     elif x=="2":
         print("退出成功！")
         client_01.close()
+import socket
 
+client_01 = socket.socket()
+client_01.connect(('10.1.30.100',9002))
+
+
+
+"""
+    一开始定义客户端去连接服务器的时候需要发送的
+    客户端点击连接按钮发送：
+    1，用户名C1=02
+    2,提取数据，将收到的信息分离，分别存起来，比如位数，num,利用分片，split分成列表，然后固定a[1]为位数，a[2]为num,a[0]是用户名，a[3]是自己的数据
+    a[4]是服务端数据，ok
+    3，编写程序，判断n输入是否==a[2],如果是就显示信息提示，退出循环，结束网络连接
+    服务器接受到了消息：
+    1，将用户名C1存起来  Sc1=C1 send=str(Sc1+"|"+位数+"|"+num+"|"+C1(传过来的数据))
+    2,input !=num,提示结果, send=send+input
+
+    
+    
+"""
+def cone(Dan):
+            global flag
+            dana=Dan
+
+
+            info = input('请输入》》》').encode("utf-8")
+
+
+            if int(info)==dana:
+                    print("you win!!")
+                    client_01.send(b'lose')
+                    client_01.close()
+                    # break
+
+            if int(info)<dana:
+                    print("you get small!!")
+                    print("waiting optinal input!!!")
+                    client_01.send(info)
+
+            if int(info)>dana:
+                    print("you get bigger!!")
+                    print("waiting optinal input!!!")
+                    client_01.send(info)
+
+            msg = client_01.recv(1024).decode('utf-8')
+            if msg == "over":
+                        # print("you lose!!")
+                        print("对方猜的是"+str(dana))
+                        print("你输了")
+                        print("--------------------")
+                        flag=0
+                        client_01.close()
+                        # socket.close()
+            # print(msg)
+            a=msg.split("|")
+            # print(a[3])
+
+
+            print("对方猜的是："+str(a[3]))
+            if int(a[3])<dana:
+                print("对方猜小了")
+            if int(a[3])>dana:
+                print("对方猜大了")
+
+
+
+
+
+if __name__ == '__main__':
+    print("选择：")
+    print("1，连接服务器")
+    print("2，退出游戏")
+
+    x=input("请选择：")
+    if x=="1":
+        myname = x.encode("utf-8")
+        client_01.send(myname)#第一次发送
+        msg = client_01.recv(1024).decode('utf-8')#第一次接收信息
+        a=msg.split("|")
+        print("这是一个"+str(a[0])+"位数")
+        print(a)
+        print(int(a[1]))
+        dana=int(a[1])
+        flag = 1
+        while flag!=0:
+            cone(dana)
+
+    elif x=="2":
+        print("退出成功！")
+        client_01.close()
+！！！！！！suc
 
